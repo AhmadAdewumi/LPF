4bfcc7f28b56073996c71ce25dab04384973078b
AhmadAdewumi <ahmadadewumi@gmail.com>
2025-07-23 15:29:27 +0100

    modified properties

--- src/main/java/com/ahmad/ProductFinder/configuration/OpenApiConfig.java
+++ src/main/java/com/ahmad/ProductFinder/configuration/OpenApiConfig.java
@@ -20,7 +20,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI defineOpenApi() {
         Server server = new Server();
-        server.setUrl("http://localhost:8080");
+        server.setUrl(url);
         server.setDescription("Development");
 
         Contact myContact = new Contact();
