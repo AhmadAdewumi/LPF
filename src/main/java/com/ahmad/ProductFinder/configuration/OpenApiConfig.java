5debb1fbddde4f6a3842c4a8c7aaf4d32fd864f5
AhmadAdewumi <ahmadadewumi@gmail.com>
2025-07-23 15:17:46 +0100

    modified properties

--- src/main/java/com/ahmad/ProductFinder/configuration/OpenApiConfig.java
+++ src/main/java/com/ahmad/ProductFinder/configuration/OpenApiConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,8 @@ import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+    @Value("${site.base.url.https}")
+    private String url;
     @Bean
     public OpenAPI defineOpenApi() {
         Server server = new Server();
